b9b362745f0d19fbe47df0070191950853dd7040
ElEdMedina21 <emedina21@alumnos.uaq.mx>
2025-03-28 14:33:58 -0600

    Set router logic and created Dashboard component

---
+++ Hackathon2025/src/components/Display.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom"
+
+export default function Display(){
+    return(
+        <div className="w-[100%] m-2">
+            <Routes>
+
+            </Routes>
+        </div>
+    )
+}
