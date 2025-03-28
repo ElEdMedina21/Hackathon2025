9bd1fdd35f53e26642ddb6d328a341d0279fcc02
ElEdMedina21 <emedina21@alumnos.uaq.mx>
2025-03-28 14:23:57 -0600

    Created directory logic

--- Hackathon2025/src/components/NavBar.jsx
+++ Hackathon2025/src/components/NavBar.jsx
@@ -1,8 +1,13 @@
 export default function NavBar(){
     return(
         <>
-            <nav className="w-full flex items-center">
-
+            <nav className="w-full flex items-center justify-between px-6 py-4 drop-shadow-md bg-[#FBFFE4]">
+                <p className="text-[#06D001] font-bold text-2xl">AjoloEnergy</p>
+                <div className="flex gap-4">
+                    <p className="text-[#059212] text-xl">General</p>
+                    <p className="text-[#059212] text-xl">Planear</p>
+                    <p className="text-[#059212] text-xl">Configurar</p>
+                </div>
             </nav>
         </>
     )
